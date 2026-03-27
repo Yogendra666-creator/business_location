2e860ec5f32cb034d8f9262e9930be700d77f0ab
Yogendra666-creator <65337439+Yogendra666-creator@users.noreply.github.com>
2026-03-27 12:20:22 +0530

    Refactor business_location.py for clarity and updates

--- business_location.py
+++ business_location.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 # Load data
 # df = pd.read_csv("Loan-Portfolio-data.csv")
-df = pd.read_csv("Loan-Portfolio-Mapping-Data-2.csv")
+df = pd.read_csv("Loan_Portfolio_Mapping_Data.csv")
 
 st.title("Loan Portfolio Map Dashboard")
 
